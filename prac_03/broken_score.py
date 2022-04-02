2cb22be0df3786ac2a98036bf352ae1b09633e7b
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-02 23:45:40 +0800

    add main function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -10,15 +10,19 @@ LIMIT_MAX = 100
 LIMIT_PASS = 50
 LIMIT_EXCELLENT = 90
 
-result = ''
-score = float(input("Enter score: "))
-if LIMIT_MIN <= score <= LIMIT_MAX:
-    if score >= LIMIT_EXCELLENT:
-        result = "Excellent"
-    elif score >= LIMIT_PASS:
-        result = "Passable"
+
+def main():
+    score = float(input("Enter score: "))
+    if LIMIT_MIN <= score <= LIMIT_MAX:
+        if score >= LIMIT_EXCELLENT:
+            result = "Excellent"
+        elif score >= LIMIT_PASS:
+            result = "Passable"
+        else:
+            result = "Bad"
     else:
-        result = "Bad"
-else:
-    result = "Invalid score"
-print(result)
+        result = "Invalid score"
+    print(result)
+
+
+main()
