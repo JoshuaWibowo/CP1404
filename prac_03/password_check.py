cdbb8f9a12209283029cecb90a8bfda78522f602
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-02 23:15:23 +0800

    refactor codes into get_password and print-asterisks function

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -7,14 +7,22 @@ PASS_MIN_LENGTH = 5
 
 def main():
     """Password checker main function"""
-    user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
+    user_password = get_password()
 
+    print_asterisks(user_password)
+
+
+def print_asterisks(user_password):
+    for char in user_password:
+        print("*", end='')
+
+
+def get_password():
+    user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
     while len(user_password) != PASS_MIN_LENGTH:
         print(f"Invalid password, must be {PASS_MIN_LENGTH} char long")
         user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
-
-    for char in user_password:
-        print("*", end='')
+    return user_password
 
 
 main()
