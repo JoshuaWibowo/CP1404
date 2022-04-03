88777776659b6ece183a48aa6f0aba2df100c9fe
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-03 12:12:57 +0800

    change randint to uniform

--- Practice & Extension Work/Practice/temps_input_generator.py
+++ Practice & Extension Work/Practice/temps_input_generator.py
@@ -5,7 +5,7 @@ input_file = open("temps_input.txt", "w")
 # no_of_temps = int(input("How many temp data? "))
 no_of_temps = 15
 while repeats != no_of_temps:
-    temp = float(random.randint(-200, 200))
+    temp = float(random.uniform(-200, 200))
     print(temp, file=input_file)
     repeats += 1
 input_file.close()
