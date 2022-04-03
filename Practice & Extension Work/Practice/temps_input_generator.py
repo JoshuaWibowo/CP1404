dff47ed14be24a5102cf04a269b120646cb04171
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-03 12:07:09 +0800

    create while loop to generate random values and save it to the txt file

---
+++ Practice & Extension Work/Practice/temps_input_generator.py
@@ -0,0 +1,11 @@
+import random
+
+repeats = 0
+input_file = open("temps_input.txt", "w")
+# no_of_temps = int(input("How many temp data? "))
+no_of_temps = 15
+while repeats != no_of_temps:
+    temp = float(random.randint(-200, 200))
+    print(temp, file=input_file)
+    repeats += 1
+input_file.close()
