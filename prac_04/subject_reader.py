5ad4a4f094003af73597d976f5d50621e8cb0944
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-11 05:33:41 +0800

    create function to display subject data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,19 +8,25 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_subject(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    subjects_data = []
+    subject_data = []
     for line in input_file:
         part = line.strip().split(',')
         part[-1] = int(part[-1])
-        subjects_data.append(part)
+        subject_data.append(part)
     input_file.close()
-    return subjects_data
+    return subject_data
+
+
+def display_subject(subject_data):
+    """Display subject data"""
+    for data in subject_data:
+        print(f"{data[0]} is taught by {data[1]:12} and has {data[2]:3} students")
 
 
 main()
