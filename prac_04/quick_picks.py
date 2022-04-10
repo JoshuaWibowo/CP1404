8b6ec53e802874b947bf89eb4573d1ffd5cb3f0a
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-11 05:03:45 +0800

    create loops to generate quick picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,3 +9,14 @@ MIN_NUMBER = 1  # Minimum value in a pick
 MAX_NUMBER = 45  # Maximum value in a pick
 
 quick_pick = int(input("How many quick picks? "))
+for pick in range(quick_pick):
+    quick_picks = []
+    for number in range(NUMBERS_IN_PICK):
+        num = random.randint(MIN_NUMBER, MAX_NUMBER)
+        while num in quick_picks:
+            num = random.randint(MIN_NUMBER, MAX_NUMBER)
+        quick_picks.append(num)
+    quick_picks.sort()
+    for value in quick_picks:
+        print(f"{value:3}", end='')
+    print()
