6558620e5eb9da9cd7eee9deeb4dd9a67dd62e50
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-11 04:45:47 +0800

    add constant values

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,5 +4,8 @@ Quick picks
 """
 import random
 
-quick_pick = int(input("How many quick picks? "))
+NUMBERS_IN_PICK = 6  # How many numbers in one line
+MIN_NUMBER = 1  # Minimum value in a line
+MAX_NUMBER = 45  # Maximum value in a line
 
+quick_pick = int(input("How many quick picks? "))
