7cd8843fa7801858070486e802c00c8b4ae8e6a5
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-10 15:34:28 +0800

    create code to get and print the first, last, smallest, largest, and the average of the numbers

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,3 +3,13 @@ numbers = []
 for number in range(total_number):
     number_input = int(input("Number: "))
     numbers.append(number_input)
+
+first_num, last_num = numbers[0], numbers[-1]
+smallest_num, largest_num = min(numbers), max(numbers)
+average_num = sum(numbers)/len(numbers)
+
+print(f"The first number is {first_num}")
+print(f"The last number is {last_num}")
+print(f"The smallest number is {smallest_num}")
+print(f"The largest number is {largest_num}")
+print(f"The average of the numbers is {average_num}")
