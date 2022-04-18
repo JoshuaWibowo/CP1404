7c09a3e9538ec1b1c62774bbbb97ba0c36231ab7
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-18 13:21:01 +0800

    add list comprehension to get the key in lower format

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,3 +10,8 @@ CODE_TO_COLOUR = {"Black": "#000000", "OrangeRed1": "#ff4500",
                   "Harlequin": "#3fff00", "NeonGreen": "#39ff14"}
 for key in CODE_TO_COLOUR:
     print(f"{key:<10} is {CODE_TO_COLOUR[key]:<}")
+
+colour_name = input("Enter colour name: ").lower()
+colour_key = [key.lower() for key in CODE_TO_COLOUR.keys()]
+print(colour_key)
+
