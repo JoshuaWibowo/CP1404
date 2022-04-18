d519f029d1e36bbb3f498b84da06263d7b26d888
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-19 00:35:20 +0800

    add code to ask if username is correct and error checking the user input

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,6 +3,8 @@ CP1404/CP5632 Practical
 Store email and name in a dictionary
 """
 
+VALID_INPUT = ["Y", "N", "", "YES", "NO"]
+
 email_to_name = {}
 user_email = input("Email: ")
 while user_email != "":
@@ -13,5 +15,10 @@ while user_email != "":
         user_name[name] = user_name[name].title()
     if len(user_name) > 1:
         user_name = " ".join(user_name)
+    user_name_isvalid = input(f"Is your name {user_name}? (Y/n)").upper()
+    while user_name_isvalid not in VALID_INPUT:
+        print("Invalid input")
+        user_name_isvalid = input(f"Is your name {user_name}? (Y/n)").upper()
+
     email_to_name[user_name] = user_email
     user_email = input("Email: ")
