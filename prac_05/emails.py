bd61867ebdcc163d6779d73e34babda010b39ab7
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-19 00:14:16 +0800

    add code to split the email and then store the name in a variable

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,4 +1,7 @@
 email_to_name = {}
 user_email = input("Email: ")
 while user_email != "":
+    user_name = user_email.split("@")
+    user_name = user_name[0]
+    print(user_name)
     user_email = input("Email: ")
