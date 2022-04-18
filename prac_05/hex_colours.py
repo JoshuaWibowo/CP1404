fcdef226088e97e54fb5460f124ddd6ed4ee3913
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-18 13:31:25 +0800

    add while loop to check user input and then if it is valid, retrieve the code from dict

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -8,10 +8,15 @@ CODE_TO_COLOUR = {"Black": "#000000", "OrangeRed1": "#ff4500",
                   "Blue1": "#0000ff", "Cyan1": "#00ffff",
                   "Gold1": "#ffd700", "GhostWhite": "#f8f8ff",
                   "Harlequin": "#3fff00", "NeonGreen": "#39ff14"}
-for key in CODE_TO_COLOUR:
+for key in CODE_TO_COLOUR:  # print all the colour
     print(f"{key:<10} is {CODE_TO_COLOUR[key]:<}")
 
-colour_name = input("Enter colour name: ").lower()
-colour_key = [key.lower() for key in CODE_TO_COLOUR.keys()]
-print(colour_key)
-
+colour_name = input("Enter colour name: ").lower()  # user input
+colour_key = [key.lower() for key in CODE_TO_COLOUR.keys()]  # create the lower key
+lower_to_upper = dict(zip(colour_key, CODE_TO_COLOUR.keys()))  # create new dict of lower and upper key
+while colour_name != "":
+    if colour_name in colour_key:
+        print(f"{lower_to_upper[colour_name]:<10} is {CODE_TO_COLOUR[lower_to_upper[colour_name]]:<}")
+    else:
+        print("Invalid colour name")
+    colour_name = input("Enter colour name: ").lower()
