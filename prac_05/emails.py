73d5b3f59b08efc15ca7430d0372770ee47790ba
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-19 00:25:59 +0800

    add code to make username in title format and join into one string

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,4 +4,8 @@ while user_email != "":
     user_name = user_email.split("@")
     user_name = user_name[0]
     user_name = user_name.split(".")
+    for name in range(len(user_name)):
+        user_name[name] = user_name[name].title()
+    if len(user_name) > 1:
+        user_name = " ".join(user_name)
     user_email = input("Email: ")
