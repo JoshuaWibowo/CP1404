1ea72411c7f3f6b2d3cc7ed4038ebf10ec3f4b3b
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-18 15:36:32 +0800

    add code to print the word count

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,5 @@ words = text.split(" ")
 word_to_count = {word: 0 for word in words}
 for word in words:
     word_to_count[word] += 1
+for key in word_to_count:
+    print(f"{key:<} : {word_to_count[key]}")
