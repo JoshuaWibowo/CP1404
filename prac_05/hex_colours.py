fc05779d114db62370a1d0d70e3da1f159b3a4b8
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-18 12:56:49 +0800

    add docstring and constant

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,10 @@
+"""
+CP1404/CP5632 Practical
+Hex colour code finder
+"""
+
+CODE_TO_COLOUR = {"Black": "#000000", "OrangeRed1": "#ff4500",
+                  "Yellow1": "#ffff00", "Green1": "#00ff00",
+                  "Blue1": "#0000ff", "Cyan1": "#00ffff",
+                  "Gold1": "#ffd700", "GhostWhite": "#f8f8ff",
+                  "Harlequin": "#3fff00", "NeonGreen": "#39ff14"}
