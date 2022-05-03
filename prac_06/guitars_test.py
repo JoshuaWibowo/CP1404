aab8dc4924a8ebb5c4a9beb672d330f811a24397
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 11:01:59 +0800

    Add objects

--- prac_06/guitars_test.py
+++ prac_06/guitars_test.py
@@ -1,3 +1,11 @@
 """CP1404/CP5632 Practical - Client code to use the Guitar class."""
 from prac_06.guitars import Guitar
 
+
+def main():
+    """Demo test code to show how to use Guitar class."""
+    guitar_one = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar_two = Guitar("Another Guitar", 2013, 16035.40)
+
+
+main()
