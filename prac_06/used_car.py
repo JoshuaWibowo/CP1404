cf6581108f7f02f73f4c78ad3fcb6e0268dd874f
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 08:25:31 +0800

    Add code to drive limo

--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -7,16 +7,18 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
-    my_limo = Car(100)
-    my_limo.add_fuel(20)
     my_car.drive(30)
-    print("fuel =", my_limo.fuel)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    my_limo = Car(100)
+    my_limo.add_fuel(20)
+    print("fuel =", my_limo.fuel)
+    my_limo.drive(115)
 
 
 main()
