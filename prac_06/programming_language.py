5174cf84d937fd75a12955e4f42cfc92a9de1cfd
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 09:26:24 +0800

    Create ProgrammingLanguage class and its init

---
+++ prac_06/programming_language.py
@@ -0,0 +1,11 @@
+"""CP1404/CP5632 Practical - Programming language class."""
+
+
+class ProgrammingLanguage:
+    """Represent programming language."""
+
+    def __init__(self, typing, reflection, year):
+        """Initialize a programming language information instance."""
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
