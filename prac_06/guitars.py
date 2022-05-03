c96c7d3052d1fd423b9e73566db436448d5c9b08
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 13:02:52 +0800

    Fix bugs in while loop

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -9,9 +9,15 @@ def main():
     guitar_name = input("Name: ")
     while guitar_name != "":
         guitar_year = input("Year: ")
-        guitar_cost = input("Cost: ")
+        guitar_cost = int(input("Cost: "))
         guitar = Guitar(guitar_name, guitar_year, guitar_cost)
         guitars.append(guitar)
+        print(f"{guitar.name} ({guitar.year}) : ${guitar.cost:,.2f} added.")
+        print()
+        guitar_name = input("Name: ")
+    print()
+    print("... \033[3msnip ...")
+    print()
 
 
 main()
