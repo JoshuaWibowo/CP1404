e2469d1b83d21965d58c1902a4badea493c41813
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 10:44:58 +0800

    Add docstring to __str__

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,11 +2,12 @@
 
 
 class Guitar:
-    """Represent guitar"""
+    """Represent guitar."""
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Print format that gives guitar information."""
         return f"{self.name} ({self.year}) : ${self.cost:,}"
