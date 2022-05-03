4b45d77874a132664fba3951a6a3dca8850ab10e
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 12:40:28 +0800

    Add while loop to get user input and append it to guitars list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,6 +4,14 @@ from prac_06.guitar import Guitar
 
 def main():
     """Demo codes to use car class."""
+    guitars = []
+    print("My guitars!")
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        guitar_year = input("Year: ")
+        guitar_cost = input("Cost: ")
+        guitar = Guitar(guitar_name, guitar_year, guitar_cost)
+        guitars.append(guitar)
 
 
 main()
