a9edadbd76f3e8a141129746af49dfb737480076
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 11:05:11 +0800

    Add code to test class methods

--- prac_06/guitars_test.py
+++ prac_06/guitars_test.py
@@ -7,5 +7,10 @@ def main():
     guitar_one = Guitar("Gibson L-5 CES", 1922, 16035.40)
     guitar_two = Guitar("Another Guitar", 2013, 16035.40)
 
+    print(f"{guitar_one.name} get_age() - Expected 100. Got {guitar_one.get_age()}")
+    print(f"{guitar_two.name} get_age() - Expected 9. Got {guitar_two.get_age()}")
+    print(f"{guitar_one.name} is_vintage() - Expected True. Got {guitar_one.is_vintage()}")
+    print(f"{guitar_two.name} is_vintage() - Expected False. Got {guitar_two.is_vintage()}")
+
 
 main()
