93e8fba10d073572c80083f32e415f0e66798d1e
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-03 08:23:03 +0800

    Add limo object

--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -7,6 +7,7 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
+    my_limo = Car(100)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
