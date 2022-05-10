21987ebb614c807333dbd361c98d70834ddad9aa
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-10 08:31:32 +0800

    Import kivy and create app class

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -5,3 +5,11 @@ Joshua Wibowo
 Started 5/10/2022
 """
 
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class MilesToKmApp(App):
+    """App that convert miles to km"""
+
