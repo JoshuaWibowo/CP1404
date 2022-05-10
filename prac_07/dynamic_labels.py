48f1ab4513d34959df001688128381c49140d1e3
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-10 18:39:58 +0800

    Create app class

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical
+Kivy GUI program to demonstrate dynamic
+Joshua Wibowo
+Started 5/10/2022
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app to demo dynamic labels creation."""
+
+
+DynamicLabelsApp().run()
