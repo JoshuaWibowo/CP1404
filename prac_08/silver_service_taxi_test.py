3c497a730204c6de0c675e53f9e6b2ff24946cc9
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 14:27:07 +0800

    Create silver_service_taxi_test.py and import the class

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,10 @@
+"""CP1404/CP5632 Practical - Client code to use the SilverServiceTaxi  class."""
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Demo test code to show how to use SilverServiceTaxi class."""
+
+
+main()
