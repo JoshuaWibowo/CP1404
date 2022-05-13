c2251a5f3d9fdefc1d2d2db4dddd4cdae122a12b
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 15:19:42 +0800

    Add and print menu

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -9,6 +9,13 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
     """Main function."""
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    menu = "q)uit, c)hoose taxi, d)rive"
+    current_taxi = None
+
+    print("Let's drive!")
+    print(menu)
 
 
 main()
