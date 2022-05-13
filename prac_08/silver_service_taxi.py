5bc34992e20aae346a0d9408b54eaae4e0ced841
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 14:14:29 +0800

    Create SilverServiceTaxi class and add __init__ function

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -4,3 +4,13 @@ SilverServiceTaxi  class
 """
 from prac_08.taxi import Taxi
 
+
+class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    price_per_km = Taxi.price_per_km
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a  SilverServiceTaxi instance, based on parent class Taxi."""
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+        self.fanciness = fanciness
