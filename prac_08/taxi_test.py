cf5d05e89dc78ac7c666613b3fba2ed9901f2604
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:25:41 +0800

    Remove price per km in client code

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -5,7 +5,7 @@ from prac_08.taxi import Taxi
 
 def main():
     """Demo test code to show how to use taxi class."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
