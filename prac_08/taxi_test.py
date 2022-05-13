4435c6146f5c29bf1ef7719f9f197150b82fd7e5
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:21:41 +0800

    Print taxi details, and begin new fare

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -7,5 +7,9 @@ def main():
     """Demo test code to show how to use taxi class."""
     my_taxi = Taxi("Prius 1", 100, 1.23)
     my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+
 
 main()
