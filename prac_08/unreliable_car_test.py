87f6e08dd426ea5adaf413b9e3ab43c5bd2c9101
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:56:53 +0800

    Add code to test new class

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,11 @@
+"""CP1404/CP5632 Practical - Client code to use the UnreliableCar class."""
+
+from prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    """Demo test code to show how to use UnreliableCar class."""
+    my_car = UnreliableCar("Car 1", 100, 1)
+
+
+main()
