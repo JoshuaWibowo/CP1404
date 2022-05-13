be67deaa8cb03652341c5c2816a5f17e2d6905f2
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:58:24 +0800

    Add code to test new class

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -5,7 +5,9 @@ from prac_08.unreliable_car import UnreliableCar
 
 def main():
     """Demo test code to show how to use UnreliableCar class."""
-    my_car = UnreliableCar("Car 1", 100, 1)
+    my_car = UnreliableCar("Car 1", 100, 70)
+    my_car.drive(50)
+    print(my_car)
 
 
 main()
