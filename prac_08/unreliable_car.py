f66a783d5c2c8e2a1b67490ebc4bdf9551577745
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:39:07 +0800

    Import car class and create basic UnreliableCar class

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,9 @@
+"""
+CP1404/CP5632 Practical
+Unreliable car class
+"""
+from prac_08.car import Car
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability."""
