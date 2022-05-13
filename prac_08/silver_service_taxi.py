31ddb5a9525bf5676ba8b922e4799a32aeb6209c
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 14:33:22 +0800

    Add code to test the class

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -7,18 +7,19 @@ from prac_08.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that includes fanciness."""
-    flagfall = 4.5
+    flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
         """Initialise a  SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.current_fare_distance = 0
         self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * self.fanciness
 
     def get_fare(self):
         """Return the price for the trip."""
-        return (super().get_fare() * self.fanciness) + self.flagfall
+        return super().get_fare() + self.flagfall
 
     def __str__(self):
         """Return a string like a Taxi but with flagfall."""
-        return f"{super().__str__()} plus flagfall of {self.flagfall}"
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -5,6 +5,10 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
     """Demo test code to show how to use SilverServiceTaxi class."""
+    my_taxi = SilverServiceTaxi("Hummer", 200, 2)
+    my_taxi.drive(18)
+    print(my_taxi.get_fare())
+    print(my_taxi)
 
 
 main()
