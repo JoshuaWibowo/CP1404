e2c30ec0e3c78cec94df6c11b5ff8457b256fdc0
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:46:37 +0800

    Copy drive method from parent class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -11,5 +11,9 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.current_fare_distance = 0
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like parent Car but add reliability variable."""
+        distance_driven = super().drive(distance)
+        return distance_driven
