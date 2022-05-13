0d734023d49e13d095e662a4b24832c7dbee42b5
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:41:50 +0800

    Create init function for the new class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -7,3 +7,9 @@ from prac_08.car import Car
 
 class UnreliableCar(Car):
     """Specialised version of a Car that includes reliability."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+        self.reliability = reliability
