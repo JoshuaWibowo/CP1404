4121f69d6197b1fb1ab964f3bcfe41d7a47e2d49
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 14:19:55 +0800

    Add get_fare method

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -7,10 +7,14 @@ from prac_08.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that includes fanciness."""
-    price_per_km = Taxi.price_per_km
+    flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         """Initialise a  SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.current_fare_distance = 0
         self.fanciness = fanciness
+
+    def get_fare(self):
+        """Return the price for the trip."""
+        return super().get_fare() * self.fanciness
