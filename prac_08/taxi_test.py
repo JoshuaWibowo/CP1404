fa1392e0a5fdb6b67ce723590cc832704b045b55
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-13 13:19:14 +0800

    Create taxi_test.py, and then create main function

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,11 @@
+"""CP1404/CP5632 Practical - Client code to use the Taxi class."""
+
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Demo test code to show how to use taxi class."""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+
+
+main()
