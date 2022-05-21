39507d780afbe9d894f37360ecece270b516897c
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-21 17:44:18 +0800

    Add for loop to get through each file in the (current) directory

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,5 +1,16 @@
-def main():
+import shutil
+import os
 
+
+def main():
+    """Program to sort files."""
+    # Change to desired directory
+    os.chdir('FilesToSort')
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
 
 
 main()
