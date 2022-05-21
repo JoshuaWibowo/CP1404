d9670825cb125ae60217b9ddcb7b8067508853a2
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-05-21 18:21:51 +0800

    Add for loop to create directory for each extension

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -4,14 +4,22 @@ import os
 
 def main():
     """Program to sort files."""
-    extension = []
+    extensions = []
     # Change to desired directory
     os.chdir(os.getcwd() + '/FilesToSort/')
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
         for index, char in enumerate(filename):
             if char == ".":
-                extension.append(filename[index + 1:])
+                extensions.append(filename[index + 1:])
+
+    extensions = set(extensions)
+
+    for extension in extensions:
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
 
 
 
